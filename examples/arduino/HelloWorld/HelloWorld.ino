989ab0456273fa7925f582c5978693d8a7ce71ea
Your Name <you@example.com>
2016-06-29 02:50:52 +0800

    helloWorld ok

--- examples/arduino/HelloWorld/HelloWorld.ino
+++ examples/arduino/HelloWorld/HelloWorld.ino
@@ -2,6 +2,14 @@
  * rosserial Publisher Example
  * Prints "hello world!"
  */
+/**
+ * Since the arduino micro / leonardo uses USB for 
+ * and its build in serial-controller both for programming and communicating, 
+ * rosserial needs the information to access the device not in the for arduino 
+ * usual serial way but with USB. So simply adding
+ */
+
+#define USE_USBCON
 
 #include <ros.h>
 #include <std_msgs/String.h>
@@ -11,12 +19,14 @@ ros::NodeHandle nh;
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 
-char hello[13] = "hello world!";
+char hello[4] = "hi\n";
 
 void setup()
 {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   nh.initNode();
   nh.advertise(chatter);
+  delay(1000);
 }
 
 void loop()

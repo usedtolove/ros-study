0752075bb19bf3cfe83a208d14e9b9e0ea72ffc3
HuJingLing <usedtolove@163.com>
2016-06-28 19:05:10 +0800

    update files

--- examples/arduino/HelloWorld/HelloWorld.ino
+++ examples/arduino/HelloWorld/HelloWorld.ino
@@ -6,7 +6,7 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
@@ -15,7 +15,6 @@ char hello[13] = "hello world!";
 
 void setup()
 {
-  Serial.begin(57600); // opens serial port, sets data rate to 9600 bps
   nh.initNode();
   nh.advertise(chatter);
 }
@@ -23,11 +22,7 @@ void setup()
 void loop()
 {
   str_msg.data = hello;
-  chatter.publish( &str_msg );
+  chatter.publish(&str_msg);
   nh.spinOnce();
-  //delay(1000);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);  
+  delay(1000);
 }
